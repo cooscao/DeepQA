a5e3d2674ec8e69973ae60e169beda6cbcaecfe6
handsomecao <cs18281902202@gmail.com>
2018-11-20 02:36:11 +0000

    add password_hash

--- models.py
+++ models.py
@@ -1,12 +1,24 @@
 from exts import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Question(db.Model):
@@ -16,6 +28,16 @@ class Question(db.Model):
     p_text = db.Column(db.Text, nullable=False)
     alternatives = db.Column(db.String(80), nullable=False)
     dataset = db.Column(db.String(20))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'q_text': self.q_text,
+            'p_text': self.p_text,
+            'alternatives': self.alternatives,
+            'dataset': self.dataset
+        }
 
 
 class History(db.Model):
--- view.py
+++ view.py
@@ -1,35 +1,92 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask.json import JSONEncoder as BaseEncoder, jsonify
+from flask._compat import text_type
 import config
 import json
 from tqdm import tqdm
 from exts import db
 from models import Question, User
+from speaklater import _LazyString
 from decorators import login_required
+
+
+class JSONEncoder(BaseEncoder):
+    def default(self, o):
+        if isinstance(o, _LazyString):
+            return text_type(o)
+        return BaseEncoder.default(self, o)
+
 
 app = Flask(__name__)
 app.config.from_object(config)
+app.json_encoder = JSONEncoder
 db.init_app(app)
+        
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        keyword = request.form.get('keyword')
+        if keyword is not None:
+            questions = Question.query.filter(Question.q_text.like(
+            '%' + keyword + '%'))
+            session['questions'] = questions
+            return redirect(url_for('list'))
+        else:
+            return redirect(url_for('index'))
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.methods == 'GET':
+    if request.method == 'GET':
         return render_template('login.html')
     else:
         username = request.form.get('username')
         password = request.form.get('password')
-        user = User.query.filter(User.username=username,
-                                User.password=password).first()
+        user = User.query.filter(User.username == username).first()
         if user:
-            session['user_id'] = user.id
-            return redirect(url_for('index'))
+            if user.verify_password(password):
+                session['user_id'] = user.id
+                return redirect(url_for('index'))
+            else:
+                flash('密码错误!')
+                return redirect(url_for('login'))
         else:
-            flash('账号或密码错误！')
+            flash('该用户名不存在!')
             return redirect(url_for('login'))
+
+@app.route('/list/', methods=['GET', 'POST'])
+def list():
+    questions = session['questions']
+    questions = jsonify(questions=[q.serialize for q in questions.all()])
+    return render_template('list.html', question=questions[0].q_text)
+
+
+@app.route('/regist/', methods=['GET', 'POST'])
+def regist():
+    if request.method == 'GET':
+        return render_template('regist.html')
+    else:
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter(User.username == username).first()
+        if user:
+            flash('该用户名已经被注册!')
+            return redirect(url_for('regist'))
+        else:
+            if password1 != password2:
+                flash('两次输入的密码不相等!')
+                return redirect(url_for('regist'))
+            else:
+                user = User(username=username, password=password1)
+                db.session.add(user)
+                db.session.commit()
+                flash('注册成功!')
+                return redirect(url_for('login'))
 
 
 
